b6d3ab2a39b3f457fa0f4881a406b488348a7695
Tejas <tejas14all@gmail.com>
2021-12-17 14:44:10 +0530

    file uploading

--- src/main/java/com/controller/UserController.java
+++ src/main/java/com/controller/UserController.java
@@ -1,10 +1,16 @@
 package com.controller;
 
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.bean.UserBean;
 
@@ -31,4 +37,32 @@ public class UserController {
 		model.addAttribute("user", user);
 		return "Home"; //
 	}
+
+	@GetMapping("/upload")
+	public String upload() {
+		return "UploadFile";
+	}
+
+	@PostMapping("/savefile")
+	public String saveFile(@RequestParam("profile") MultipartFile file) {
+
+		System.out.println(file.getOriginalFilename());
+
+		String masterPath = "d:\\abc\\";
+
+		try {
+			File myFile = new File(masterPath, file.getOriginalFilename());
+			myFile.createNewFile();
+			byte b[] = file.getBytes();
+			FileUtils.writeByteArrayToFile(myFile, b);
+			System.out.println("file uploading.....done....");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return "UploadFile";
+
+	}
+
 }
